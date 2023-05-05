396d254ebfd1802c4fc6f59e010dc83079afbcac
re-gis <irumvaregisdmc@gmail.com>
2023-05-05 22:32:27 +0200

    Creating a document

--- index.js
+++ index.js
@@ -4,20 +4,86 @@ const connectDatabase = require("./config/mongodb/db");
 const app = express();
 require("dotenv").config();
 const fileUploader = require("express-fileupload");
-const conn = require("./config/mysql/mysql");
+const mysql = require("mysql");
 const connectDB = require("./config/mysql/mysql");
-const fileUpload = require("express-fileupload");
 const currencyRouter = require("./routes/currency.routes");
-const documentRouter = require("./routes/documents.routes");
+// const documentRouter = require("./routes/documents.routes");
+const cors = require("cors");
+const multer = require("multer");
+const bodyParser = require("body-parser");
+const protect = require("./middlewares/userAuth");
 
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "re_pro",
+});
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Connect database
-connectDatabase(); // Mongobd
+connectDatabase(); // MongoDB
 connectDB(); // MySQL
 
+/* Upload a document
+   ----------------- */
+
+// Multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    const ext = file.originalname.split(".");
+    const e = ext[ext.length - 1];
+    // console.log(ext[ext.length-1])
+    cb(null, `${file.originalname}.${e}`);
+  },
+});
+
+// Upload a document
+const upload = multer({ storage: storage }).single("file");
+
+app.post("/api/docs/:user/create", protect, (req, res) => {
+  if (!user) return res.status(400).send({ message: "User not found!" });
+
+  if (user.name !== req.params.user)
+    return res.status(404).send({ message: "Cannot perform this action!" });
+  // upload a word document
+
+  upload(req, res, (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).send({ message: "Error uploading file" });
+    }
+
+    // Save the link to database
+    const path = __dirname + "\\" + req.file.path;
+    const receiver = req.body.receiver;
+    const reporter = req.params.user;
+    const details = req.body.details;
+    const subject = req.body.subject;
+    const doc_name = Date.now() + req.file.originalname;
+    const church = req.body.church;
+
+    // console.log({path, receiver, reporter, details, subject})
+    // save to database
+    const sql = `INSERT INTO documents (receiver, reporter, details, subject, path, doc_name, church) VALUES ('${receiver}', '${reporter}', '${details}', '${subject}', '${path}', '${doc_name}', '${church}')`;
+    conn.query(sql, (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(400).send({ message: "Error saving to database" });
+      }
+      console.log(result);
+      res.send({ message: "File uploaded successfully" });
+    });
+  });
+});
+
+// Use cors
+app.use(cors());
 
 // Fileuploader
 app.use(
@@ -31,10 +97,10 @@ app.use(
 app.use("/api/users", userRouter);
 
 // Currency apis
-app.use('/api/currency', currencyRouter)
+app.use("/api/currency", currencyRouter);
 
 // Document apis
-app.use('/api/document', documentRouter)
+// app.use("/api/document", documentRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`server listening port ${process.env.PORT}`);
